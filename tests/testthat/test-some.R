c5fa344096ca89760d2e4b79696a616c25c82f65
TianHao Liu <65566816+txl646@users.noreply.github.com>
2020-06-10 11:43:44 -0400

    Update test-some.R

--- tests/testthat/test-some.R
+++ tests/testthat/test-some.R
@@ -6,3 +6,7 @@ test_that("stops non-numeric", {
   expect_error(some(c(2,3,"i like chocolate milk",5)),
                "This argument is a character but should be a numeric vector")
 })
+
+test_that("stops NA", {
+  expect_equal((some(c(1,NA,3))), "Missing values are not allowed")
+})
