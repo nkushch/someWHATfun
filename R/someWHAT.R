6b2686bb5977650293e8eaa2438c1401a99505da
chol3ra <65583616+chol3ra@users.noreply.github.com>
2020-06-10 11:34:29 -0400

    Added the missing value statement and corrected syntax for stop messages

--- R/someWHAT.R
+++ R/someWHAT.R
@@ -2,10 +2,13 @@
 
 some <- function(x){
   if((is.character(x))){
-    stop("This argument is a character but should be a numeric vector", call. = FALSE)
+    stop("This argument `x` is a character, but it should be numeric", call. = FALSE)
   }
   if((!is.numeric(x))){
-    stop("The argument should be a numeric vector", call. = FALSE)
+    stop("The argument `x` should be numeric", call. = FALSE)
+  }
+  if (any(is.na(x))) {
+    stop("Missing values are not allowed", call. = FALSE)
   }
   sum(x)
 }
