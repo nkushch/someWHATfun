a0219bc754b071904b2cea09cc89a341d29c54e1
TianHao Liu <65566816+txl646@users.noreply.github.com>
2020-06-10 11:44:17 -0400

    Update some.R

--- R/some.R
+++ R/some.R
@@ -1,17 +1,14 @@
-#' Title Sum of Vectors
-#'
-#' @param x
-#'
-#' @return sum of the values
-#' @export some
-#'
-#' @examples some(c(1, 2))
+# Calculate a sum which takes in a vector and returns the total
+
 some <- function(x){
   if((is.character(x))){
     stop("This argument is a character but should be a numeric vector", call. = FALSE)
   }
   if((!is.numeric(x))){
     stop("The argument should be a numeric vector", call. = FALSE)
+  }
+  if (any(is.na(x))) {
+    stop("Missing values are not allowed", call. = FALSE)
   }
   sum(x)
 }
